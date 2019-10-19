6ecfef717c1daf1c6a6c7bf918becffbc04bb1ec
Cuicui <yan_mw@163.com>
2019-10-19 08:41:16 +0800

    完善详情页

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -10,7 +10,7 @@
     <Loading v-if="isLoading"/>
     <div v-else id="content" class="contentDetail">
       <div class="detail_list">
-        <div class="detail_list_bg"></div>
+        <div class="detail_list_bg" :style="{'background-image': 'url('+detailMovie.img.replace(/w\.h/,'/148.208/')+')'}"></div>
         <div class="detail_list_filter"></div>
         <div class="detail_list_content">
           <div class="detail_list_img">
@@ -125,7 +125,7 @@
   .detail_list .detail_list_bg {
     width: 100%;
     height: 100%;
-    background: url(../../../static/images/movie_1.jpg) 0 40%;
+    /*background: url(../../../static/images/movie_1.jpg) 0 40%;*/
     filter: blur(20px);
     background-size: cover;
     position: absolute;
